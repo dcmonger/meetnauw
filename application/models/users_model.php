67444e6e646a77eb8232c623283dd72799a781aa
Johnny Kim <junghun213@yahoo.com>
2013-07-30 16:34:52 -0700

    added a model

---
+++ application/models/users_model.php
@@ -0,0 +1,24 @@
+<?php
+
+class User_model extends CI_Model
+{
+	public function get_user($user)
+	{
+		return $this->db->where('email', $user[]'email')
+						->where('password', $user['password'])
+						->get("users")
+						->row();
+
+		return $this->db-?query("SELECT *
+								 FROM users
+								 WHERE email = '{$user['email']}'
+								 AND password = '{$user['password']}'"
+								 )->row();				
+	}
+	public function register_user($user)
+	{
+		return $this->db->insert('users', $user);
+	}
+}
+
+?>
