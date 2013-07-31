d9d248391c97c13f924589b18c3ac059da0d49aa
Brea <breatortilla@gmail.com>
2013-07-31 16:53:57 -0700

    Changed over to bootstrap, added nav, simplified pages

---
+++ application/views/partial_top.php
@@ -0,0 +1,8 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>Meetnauw</title>
+	<link href="http://netdna.bootstrapcdn.com/bootstrap/3.0.0-rc1/css/bootstrap.min.css" rel="stylesheet">
+	<script src="http://netdna.bootstrapcdn.com/bootstrap/3.0.0-rc1/js/bootstrap.min.js"></script>
+	
