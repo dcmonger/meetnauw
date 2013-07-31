f7b93b355369edfbfab8c1919009fcf07997e823
Johnny Kim <junghun213@yahoo.com>
2013-07-30 21:34:00 -0700

    added functions get_event and get_likes

--- application/models/users_model.php
+++ application/models/users_model.php
@@ -6,7 +6,7 @@ class User_model extends CI_Model
 	{
 		return $this->db->where('email', $user['email'])
 						->where('password', $user['password'])
-						->get("users")
+						->get('users')
 						->row();
 
 		return $this->db->query("SELECT *
@@ -18,6 +18,28 @@ class User_model extends CI_Model
 	public function register_user($user)
 	{
 		return $this->db->insert('users', $user);
+	}
+	public function get_event($events)
+	{
+		return $this->db->where('events', $events['user'])
+						->get('events')
+						->row();  
+
+		return $this->db->query("SELECT *
+								 FROM events
+								 WHERE events = '{$events['user']}'"
+								 )->row();	
+	}
+	public function get_likes($likes)
+	{
+		return $this->db->where('likes', $likes['user'])
+						->get('likes')
+						->row();
+
+		return $this->db->query("SELECT *
+								 FROM likes
+								 WHERE likes = '{$likes['user']}'"
+								 )->row();					
 	}
 }
 
